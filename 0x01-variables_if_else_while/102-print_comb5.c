a15ea5f2d4a677271865d535206de0df2bef5abe
ifydivinelove <ifydivinelove@gmail.com>
2023-04-30 19:05:16 +0100

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,19 +12,18 @@ int main(void)
 
 	for (i = 0; i <= 98; i++)
 	{
-		for (j = i + 1; j <= 99; j++)
-		{
-			putchar((i / 10) + '0');
-			putchar((j % 10) + '0');
-			putchar(' ');
-			putchar((j / 10) + '0');
-			putchar((j % 10) + '0');
-			if (i == 98 && j == 99)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
+	for (j = i + 1; j <= 99; j++)
+	{
+	putchar((i / 10) + '0');
+	putchar((i % 10) + '0');
+	putchar(' ');
+	putchar((j / 10) + '0');
+	putchar((j % 10) + '0');
+	if (i == 98 && j == 99)
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
 	}
 	putchar('\n');
 	return (0);
