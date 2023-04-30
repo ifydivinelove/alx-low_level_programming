0c7524b82afbd0c214622c95535923c841091e06
ifydivinelove <ifydivinelove@gmail.com>
2023-04-30 18:53:17 +0100

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,22 +8,19 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 100; i++)
+	for (i = 0; i <= 98; i++)
 	{
-		for (j = 0; j < 100; j++)
+		for (j = i; j <= 99; j++)
 		{
-			if (i < j)
+			putchar((i / 10) + '0');
+			putchar((j % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			if (i == 98 && j == 99)
 			{
-				putchar((i / 10) + 48);
-				putchar((j % 10) + 48);
+				putchar(',');
 				putchar(' ');
-				putchar((j / 10) + 48);
-				putchar((j % 10) + 48);
-				if (i != 98 || j != 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
 			}
 		}
 	}
