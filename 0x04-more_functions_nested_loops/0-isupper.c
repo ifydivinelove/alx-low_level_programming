0345ddaf03e82f8c89f2d64e8a9c85058d17b545
ifydivinelove <ifydivinelove@gmail.com>
2023-05-03 17:41:42 +0100

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isupper: checks for lowercase character
+ * _isupper - checks for lowercase character
  * @c: The character to be checked
  * Return: 1 for uppercase character or 0 for anything else
  */
